3f2e18b201ab78580d87ff7d9a27351315979937
huyue <linbeithree@163.com>
2017-07-10 17:44:15 +0800

    hello: modify some function for DN Ticket: #2 commit this

--- helloworld.c
+++ helloworld.c
@@ -1,3 +1,10 @@
-void main(void){
-	printf("\nhelloworld");
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+	printf("argc is %d\n", argc);
+	printf("argv[0] = %s,  argv[1] = %s,argv[2] = %s,argv[3] = %s,argv[4] = %s\n",argv[0], argv[1], argv[2], argv[3], argv[4]);
+	printf("\nhelloworld\n");
+	return 0;
 }
